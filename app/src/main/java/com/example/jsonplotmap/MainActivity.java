0073154ba25b6d4a5be2dad33a58f33b5da27ab5
anishs95 <48278960+anishs95@users.noreply.github.com>
2020-02-24 23:21:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jsonplotmap/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.jsonplotmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MapFragment fragment = new MapFragment();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
